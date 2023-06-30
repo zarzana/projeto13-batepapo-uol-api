c993b25daa128e61ecc8b9302b12abd3c55d1488
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-06-30 01:10:45 -0300

    Implemented POST /participants.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
+import joi from 'joi';
+import dayjs from 'dayjs';
 
 // app initialization
 const app = express();
@@ -17,6 +19,45 @@ let db;
 mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message));
+
+// endpoints
+app.post('/participants', async (req, res) => {
+
+    // joi validation
+    const participantSchema = joi.object({
+        name: joi.string().required()
+    });
+    const validation = participantSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    // db operations
+    try {
+        // check for username duplicate
+        const participant = await db.collection('participants').findOne({ name: req.body.name });
+        if (participant) return res.status(409).send('Nome já existe.');
+        // add user to participants collection
+        await db.collection('participants').insertOne({
+            name: req.body.name,
+            lastStatus: dayjs().valueOf()
+        });
+        // add login message to messages collection
+        await db.collection('messages').insertOne({
+            from: req.body.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        });
+        // status
+        res.sendStatus(201);
+    } catch (err) {
+        res.sendStatus(500).send(err.message);
+    }
+
+})
 
 // listen
 const PORT = 5000;
